a1498edf086e9ad70a567fe0affe386e0ad98350
John Dewey <john@dewey.ws>
2015-12-01 23:08:54 -0800

    Solved part 2 of problem 2

--- 2/main.py
+++ 2/main.py
@@ -1,12 +1,19 @@
 total = 0
+ribbon = 0
 with open('input', 'r') as f:
     for line in f.read().splitlines():
         length, width, height = list(map(int, line.split('x')))
 
-        side1 = (length * width)
-        side2 = (width * height)
-        side3 = (height * length)
+        side1 = length * width
+        ribbon1 = length + length + width + width
+        side2 = width * height
+        ribbon2 = width + width + height + height
+        side3 = height * length
+        ribbon3 = height + height + length + length
+        bow = length * width * height
 
+        ribbon += bow + min(ribbon1, ribbon2, ribbon3)
         total += 2 * side1 + 2 * side2 + 2 * side3 + min(side1, side2, side3)
 
-print total
+print 'paper: {0}'.format(total)
+print 'ribbon: {0}'.format(ribbon)
