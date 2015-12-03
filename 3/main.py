895035e6ae2fae536c49cf4198608ce19d16ac4f
John Dewey <john@dewey.ws>
2015-12-03 14:26:12 -0800

    Solved part 2 of problem 3

--- 3/main.py
+++ 3/main.py
@@ -1,22 +1,33 @@
-from collections import defaultdict
+def open_file():
+    with open('input', 'r') as f:
+        return f.read()
 
-x = 0
-y = 0
-positions = defaultdict(int)
-positions['0:0'] = 1
+def get_directions(instructions):
+    x = 0
+    y = 0
+    current = (x, y)
+    positions = set()
+    positions.add(current)
+    for direction in list(instructions):
+        if direction == '^':
+            y += 1
+        elif direction == 'v':
+            y -= 1
+        elif direction == '>':
+            x += 1
+        elif direction == '<':
+            x -= 1
+        current = (x, y)
+        positions.add(current)
+    return positions
 
-with open('input', 'r') as f:
-    for instructions in f:
-        for direction in list(instructions):
-            if direction == '^':
-                y += 1
-            elif direction == 'v':
-                y -= 1
-            elif direction == '>':
-                x += 1
-            elif direction == '<':
-                x -= 1
-            key = '{}:{}'.format(x, y)
-            positions[key] += 1
+instructions = open_file()
+positions = get_directions(instructions)
 
-    print 'visited: {0}'.format(len(positions))
+print 'visited: {0}'.format(len(positions))
+assert len(positions) == 2572
+
+positions = get_directions(instructions[::2]) | get_directions(instructions[1::2])
+
+print 'visited: {0}'.format(len(positions))
+assert len(positions) == 2631
