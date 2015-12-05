64738a96b262563a73a6ca79829e7714e9cc26da
John Dewey <john@dewey.ws>
2015-12-05 14:52:54 -0800

    Solved part 2 of problem 5

--- 5/main.py
+++ 5/main.py
@@ -9,27 +9,34 @@ def has_consecutive_letters(string):
     return re.search(r'([a-z])\1', string)
 
 
+def has_alternating_pairs(string):
+    return re.search(r'([a-z])[a-z]\1', string)
+
+
+def has_non_overlapping_pairs(string):
+    return re.search(r'([a-z][a-z]).*\1', string)
+
+
 def has_naughty_letters(string):
     return re.search(r'ab|cd|pq|xy', line)
 
 
 def nice_string(string):
-    return (has_three_vowels(string) and
-            has_consecutive_letters(string) and not
-            has_naughty_letters(string))
+    return (has_three_vowels(string) and has_consecutive_letters(string) and
+            not has_naughty_letters(string))
 
 
-nice = 0
-naughty = 0
+def new_nice_string(string):
+    return (has_non_overlapping_pairs(string) and
+            has_alternating_pairs(string))
+
 with open('input', 'r') as f:
-    for line in f.read().splitlines():
-        if nice_string(line):
-            nice += 1
-        else:
-            naughty += 1
+    lines = f.read().splitlines()
+    nice = [line for line in lines if nice_string(line)]
+    new_nice = [line for line in lines if new_nice_string(line)]
 
-print 'nice: {0}'.format(nice)
-assert nice == 255
+    print 'nice: {0}'.format(len(nice))
+    assert 255 == len(nice)
 
-print 'naughty: {0}'.format(naughty)
-assert naughty == 745
+    print 'new nice: {0}'.format(len(new_nice))
+    assert 55 == len(new_nice)
