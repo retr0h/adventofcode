b76d6385df6e9283c786ba4eae487e5927c5d023
נυαη נυαηѕση <john@dewey.ws>
2023-12-08 13:16:29 -0800

    commit stray file from last year

--- 2020/3/main.py
+++ 2020/3/main.py
@@ -11,3 +11,13 @@ with open("input", "r") as f:
         x += 3
 
     print(tree_count)
+
+
+
+
+    #  Right 1, down 1.
+    #  Right 3, down 1. (This is the slope you already checked.)
+    #  Right 5, down 1.
+    #  Right 7, down 1.
+    #  Right 1, down 2.
+
